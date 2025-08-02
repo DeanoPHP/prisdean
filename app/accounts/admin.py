555f1e2f87083f9dddd269a82c1a267422a1b2c5
DeanoLark <deanolark@gmail.com>
2025-08-02 23:27:49 +0100

    Registered UserProfile and intergrate with CustomUser admin

--- app/accounts/admin.py
+++ app/accounts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+from userprofile.models import UserProfile
 
-# Register your models here.
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+    verbose_name_plural = 'Profile'
+
+class CustomUserAdmin(UserAdmin):
+    inlines = (UserProfileInline,)
+    list_display = ('email', 'is_staff', 'is_active')
+    search_fields = ('email',)
+    ordering = ('email',)
+
+admin.site.register(CustomUser, CustomUserAdmin)
