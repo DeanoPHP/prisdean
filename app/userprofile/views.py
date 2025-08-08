62bf7a2b0be82979c867f4c6d12df9a03b89ab95
DeanoLark <deanolark@gmail.com>
2025-08-08 01:06:13 +0100

    Added style to mobile navbar;Started views for profile CRUD functionality

--- app/userprofile/views.py
+++ app/userprofile/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import (
+    TemplateView, 
+    UpdateView, 
+    DeleteView,
+)
 from django.contrib.auth.mixins import LoginRequiredMixin
+from accounts.forms import CustomSignupForm
+from django.urls import reverse_lazy
 
 
 class ProfileView(LoginRequiredMixin, TemplateView):
@@ -13,6 +19,21 @@ class ProfileView(LoginRequiredMixin, TemplateView):
         context['profile'] = user.profile
         
         return context
+    
+    
+class UpdateProfile(LoginRequiredMixin, UpdateView):
+    template_name = 'userProfile/update_profile.html'
+    success_url = reverse_lazy('profile')
+    
+    
+    
+class DeleteProfile(LoginRequiredMixin, DeleteView):
+    """
+    Need to create a signal that deletes the user if
+    profile is deleted although I think the model 
+    already handles this.
+    """ 
+    pass
     
 
 
